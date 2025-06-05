0b20484db90d75942ab5fa7c2fa0d29bf794bbb5
sebgrgs <sebastien.georges11@gmail.com>
2025-06-05 09:24:34 +0200

    Add initial game files including README, level map, and tilesets

---
+++ assets/tilesets/spring_tileset.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="spring_tileset" tilewidth="16" tileheight="16" tilecount="220" columns="11">
+ <properties>
+  <property name="collides" type="bool" value="true"/>
+ </properties>
+ <image source="Downloads/fourSeasonsPlatformer_ [tileset, version 2.0]/fourSeasonsPlatformer_ [tileset, version 2.0]/midground_/spring_.png" width="176" height="320"/>
+</tileset>
